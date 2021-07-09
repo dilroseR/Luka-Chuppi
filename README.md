0972163cfc0621305bbe2e49ca93351756c239ab
Dilrose Reji <dilrose2000@gmail.com>
2021-07-09 21:40:31 +0530

    updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Luka-Chuppi
 Data Hiding in Image-Steganography
+
+Steganography is a technique of hiding a message within image or audio files. It means of storing data in a way that it hides the existence of them. We use this method when we don't want anyone else to see the message other than the sender and the intended receiver. This process helps in enhancing the security of the message.
+
+In this project, I've used the module named **cryptosteganography** module in python
+
+## Installation ##
+To install the package on windows, run the command below in the cmd
+
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import cv2
+from cryptosteganography import CryptoSteganography
+import getpass
+crypto_steganography = CryptoSteganography('')
 
-x = cv2.imread("img.jpeg")
-i = x.shape[0]
-j = x.shape[1]
-print(i,j)
+# Save the encrypted file inside the image
+opt = int(input("1. Encode \t 2. Decode \t 3. Quit :: "))
 
-# encrypt image
-key = int(input("Enter secret key: "))
-msg = input("Enter message that you want to send: ")
+while opt!=3:
+    if opt==1:
+        msg = input("Enter the message to be hidden: ")
+        key = getpass.getpass("Enter secret key: ")
+        crypto_steganography.hide('img.jpeg', 'output.png', msg)
+        print("Data successfully hidden!")
+    else:
+        key1 = getpass.getpass("Enter secret key to retrieve the data: ")   # Decryption of data from the image
+        if key == key1:
+            secret = crypto_steganography.retrieve('output.png')
+            print("Encoded data: ", secret)         # Secret message displayed
+        else:
+            print("The entered key is INCORRECT")
+            print("Please enter the correct secret key")  
+
+    print(" ")
+    opt= int(input("1. Encode \t 2. Decode \t 3. Quit :: "))
